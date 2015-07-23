f1cec2ad3d875fd8e3057be8b8149fa44143a005
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2015-07-24 01:51:45 +0900

    Fix style
    Separate every 3 digits in the integer portion of a number with
    underscores(_).

--- check/stock_check.rb
+++ check/stock_check.rb
@@ -5,9 +5,9 @@ puts stock.code
 puts stock.market
 puts stock.unit
 
-stock.add_price('2011-07-01', 402, 402, 395, 397, 17495700)
-stock.add_price('2011-07-04', 402, 404, 400, 403, 18819300)
-stock.add_price('2011-07-05', 402, 408, 399, 401, 20678000)
+stock.add_price('2011-07-01', 402, 402, 395, 397, 17_495_700)
+stock.add_price('2011-07-04', 402, 404, 400, 403, 18_819_300)
+stock.add_price('2011-07-05', 402, 408, 399, 401, 20_678_000)
 puts stock.prices[0][:date]
 puts stock.prices[1][:open]
 puts stock.prices[2][:high]
