a8bcf85ec8c3b8368978e91cd56dfdbef01ba582
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2015-07-24 00:45:57 +0900

    Add some instance methods to Stock

--- lib/stock.rb
+++ lib/stock.rb
@@ -9,4 +9,43 @@ class Stock
     @prices = []
     @price_hash = {}
   end
+
+  # Add daily stock price
+  def add_price(date, open, high, low, close, volume)
+    @prices << { date: date,
+                 open: open,
+                 hight: high,
+                 low: low,
+                 close: close,
+                 volume: volume }
+  end
+
+  # Get array of date, open, high, low, close, or volume values
+  def map_prices(price_name)
+    @price_hash[price_name] ||= @prices.map { |price| price[price_name] }
+  end
+
+  def dates
+    map_prices(:date)
+  end
+
+  def open_prices
+    map_prices(:open)
+  end
+
+  def high_prices
+    map_prices(:high)
+  end
+
+  def low_prices
+    map_prices(:low)
+  end
+
+  def close_prices
+    map_prices(:close)
+  end
+
+  def volumes
+    map_prices(:volume)
+  end
 end
