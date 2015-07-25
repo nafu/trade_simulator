7f443d63574be654683c1102955d1ac47d3e954a
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2015-07-25 14:51:04 +0900

    Update get_price_data to get latest price data

--- bin/get_price_data.rb
+++ bin/get_price_data.rb
@@ -1,10 +1,10 @@
 require './lib/stock_data_getter'
 
 from = '1983/01/4'
-to   = '2011/06/30'
+to   = '2015/07/24'
 market = :t
 sdg = StockDataGetter.new(from, to, market)
 
-(2499..9999).each do |code|
-  sdg.get_price_data(code)
+(1300..9999).each do |code|
+  sdg.update_price_data(code)
 end
